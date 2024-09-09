345e2cbe8676a29b1d09920e8431fc4e893e9f65
Niharika Thakur <thakurniharika2003@gmail.com>
2024-09-10 00:57:26 +0530

    changes made in frontpage

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import FirstSelection from '../Options/FirstSelection'
 import SecondSelection from '../Options/SecondSelection'
 import ThirdSelection from '../Options/ThirdSelection'
+import TopNews from '../Options/TopNews'
 
 const MainPage = () => {
   return (
     <div>
+         <TopNews/>
          <FirstSelection/>
          <SecondSelection/>
          <ThirdSelection/>
---
+++ src/components/Options/TopNews.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './topnews.css';
+
+const TopNews = () => {
+  const gridItems = Array.from({ length: 8 }, (_, index) => `Item ${index + 1}`);
+
+  return (
+    <div className="container-layout">
+      <div className="left-container">Left Container (50%)</div>
+      <div className="right-container">
+        <div className="top-right-container">
+          {gridItems.slice(0, 4).map((item, index) => (
+            <div key={index} className="grid-item">{item}</div>
+          ))}
+        </div>
+        <div className="bottom-right-container">
+          {gridItems.slice(4, 8).map((item, index) => (
+            <div key={index} className="grid-item">{item}</div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TopNews;
